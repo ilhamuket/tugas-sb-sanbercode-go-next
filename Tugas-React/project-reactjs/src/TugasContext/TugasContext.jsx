1c0e95d501249218a73d15e1d3fdcc0ba5043303
Muhammad Ilham <ilhamgallagher633@gmail.com>
2024-07-18 00:07:44 +0700

    update ui

--- Tugas-React/project-reactjs/src/TugasContext/TugasContext.jsx
+++ Tugas-React/project-reactjs/src/TugasContext/TugasContext.jsx
@@ -8,8 +8,9 @@ const TugasContext = () => {
         <BookProvider>
             <div className="tugas-crud-axios">
                 <h1>Tugas CRUD Books</h1>
-                <div className="swagger-link-container">
-                    <a href="https://tugas-sb-sanbercode-go-next-2024-xnnms-projects.vercel.app/swagger/index.html" target="_blank" rel="noopener noreferrer" className="swagger-link">Swagger Backend</a>
+                <div className="button-container">
+                    <a href="https://tugas-sb-sanbercode-go-next-2024-xnnms-projects.vercel.app/swagger/index.html" target="_blank" rel="noopener noreferrer" className="swagger-link">Backend Live</a>
+                    <a href="https://tugas-sb-sanbercode-go-next-2024-alu5.vercel.app" target="_blank" rel="noopener noreferrer" className="swagger-link">React Live</a>
                 </div>
                 <BookForm />
                 <BookTable />
